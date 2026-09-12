480c75c694a2390418595e49d8f89cc5c578751e
prchdev <36146003+prchdev@users.noreply.github.com>
2026-09-12 18:41:13 +0530

    Updated MobileForgotPassword.tsx

--- src/pages/mobile/MobileForgotPassword.tsx
+++ src/pages/mobile/MobileForgotPassword.tsx
@@ -129,7 +129,7 @@ export default function MobileForgotPassword() {
       behavior={Platform.OS === 'ios' ? 'padding' : undefined}
       style={{ flex: 1 }}
     >
-    <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }} className="flex-1 bg-gray-50 px-4 py-6" keyboardShouldPersistTaps="handled" keyboardShouldDismissOnDrag="always" showsVerticalScrollIndicator={false}>
+    <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }} className="flex-1 bg-gray-50 px-4 py-6" keyboardShouldPersistTaps="handled" showsVerticalScrollIndicator={false}>
       <View className="max-w-sm w-full self-center">
         <View className="items-center mb-4">
           <Image
