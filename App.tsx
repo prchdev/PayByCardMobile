a72e72dbaa5af07e1287a65bd0a02830bd7ae192
prchdev <36146003+prchdev@users.noreply.github.com>
2026-09-12 19:30:52 +0530

    Fix: Add NativeWind CSS import to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 /// <reference types="nativewind/types" />
 
+import './src/native-styles.css';
 import { useRef, useEffect, useState } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer, NavigationContainerRef } from '@react-navigation/native';
