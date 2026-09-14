375c2816e104788f6235c46b73485d172c4cd862
prchdev <36146003+prchdev@users.noreply.github.com>
2026-09-14 12:38:40 +0530

    Use expo-build-properties for R8 minification

--- plugins/withProguard.js
+++ plugins/withProguard.js
@@ -1,10 +1,9 @@
-const { withAppBuildGradle, withProjectBuildGradle } = require('@expo/config-plugins');
 const { withDangerousMod } = require('@expo/config-plugins');
 const fs = require('fs');
 const path = require('path');
 
 const withProguard = (config) => {
-  const withProguardRules = withDangerousMod(config, [
+  return withDangerousMod(config, [
     'android',
     async (modConfig) => {
       const projectRoot = modConfig.projectRoot || config.projectRoot || process.cwd();
@@ -20,52 +19,6 @@ const withProguard = (config) => {
       return modConfig;
     },
   ]);
-
-  const withGradleProps = withDangerousMod(withProguardRules, [
-    'android',
-    async (modConfig) => {
-      const projectRoot = modConfig.projectRoot || config.projectRoot || process.cwd();
-      const propsFile = path.join(projectRoot, 'android', 'gradle.properties');
-      let props = '';
-
-      if (fs.existsSync(propsFile)) {
-        props = fs.readFileSync(propsFile, 'utf8');
-      }
-
-      if (!props.includes('android.suppressUnsupportedCompileWarnings')) {
-        props += '\nandroid.suppressUnsupportedCompileWarnings=true\n';
-      }
-      if (!props.includes('android.experimental.enableNewAgpBase')) {
-        props += 'android.experimental.enableNewAgpBase=true\n';
-      }
-
-      fs.writeFileSync(propsFile, props);
-      return modConfig;
-    },
-  ]);
-
-  return withAppBuildGradle(withGradleProps, (modConfig) => {
-    let buildGradle = modConfig.modResults.contents;
-
-    if (!buildGradle.includes('minifyEnabled true')) {
-      const releaseBlock = `    buildTypes {
-        release {
-            minifyEnabled true
-            shrinkResources true
-            proguardFiles getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
-        }
-    }`;
-
-      buildGradle = buildGradle.replace(
-        /android\s*\{/,
-        `android {
-${releaseBlock}`
-      );
-    }
-
-    modConfig.modResults.contents = buildGradle;
-    return modConfig;
-  });
 };
 
 module.exports = withProguard;
